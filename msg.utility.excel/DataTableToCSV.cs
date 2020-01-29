6f84dc2070205e4a5a5f81412394640e7ac4e85f
oyvind <oyvind@raincode.no>
2020-01-29 11:26:25 +0100

    initial checkin

---
+++ msg.utility.excel/DataTableToCSV.cs
@@ -0,0 +1,35 @@
+﻿using System.Data;
+using System.IO;
+using System.Text;
+
+namespace msg.utility.excel
+{
+    public static class DataTableToCSV
+    {
+        public static MemoryStream ToCSV(this DataTable table)
+        {
+            var result = new StringBuilder();
+            for (int i = 0; i < table.Columns.Count; i++)
+            {
+                result.Append(table.Columns[i].ColumnName);
+                result.Append(i == table.Columns.Count - 1 ? "\n" : ",");
+            }
+
+            foreach (DataRow row in table.Rows)
+            {
+                for (int i = 0; i < table.Columns.Count; i++)
+                {
+                    result.Append(row[i]);
+                    result.Append(i == table.Columns.Count - 1 ? "\n" : ",");
+                }
+            }
+
+            return GenerateStreamFromString(result.ToString());
+        }
+
+        private static MemoryStream GenerateStreamFromString(string s)
+        {
+            return new MemoryStream(Encoding.UTF8.GetBytes(s ?? ""));
+        }
+    }
+}
---
+++ msg.utility.excel/DataTableToExcel.cs
@@ -0,0 +1,47 @@
+﻿using OfficeOpenXml;
+using System.Data;
+using System.IO;
+
+namespace msg.utility.excel
+{
+
+    public static class DataTableToExcel
+    {
+        public static MemoryStream ToExcel(this DataTable table, string worksheetName)
+        {
+            var ms = new MemoryStream();
+
+            using (ExcelPackage pck = new ExcelPackage())
+            {
+                ExcelWorksheet ws = pck.Workbook.Worksheets.Add(worksheetName);
+                ws.Cells["A1"].LoadFromDataTable(table, true);
+                pck.Save();
+                pck.SaveAs(ms);
+            }
+
+            return ms;
+        }
+
+        public static MemoryStream ToExcel(this DataTable[] tables, string[] worksheetNames)
+        {
+            var ms = new MemoryStream();
+
+            using (ExcelPackage pck = new ExcelPackage())
+            {
+                for (int i = 0; i < tables.Length; i++)
+                {
+                    var worksheetName = worksheetNames[i];
+                    var table = tables[i];
+
+                    ExcelWorksheet ws = pck.Workbook.Worksheets.Add(worksheetName);
+                    ws.Cells["A1"].LoadFromDataTable(table, true);
+                }
+
+                pck.Save();
+                pck.SaveAs(ms);
+            }
+
+            return ms;
+        }
+    }
+}
